19d07f7299f08d889bb57d0227252ed4abf4cbdb
John <112360755+johnshu2022@users.noreply.github.com>
2023-10-14 03:17:17 +0000

    Did additional practice in C file

--- practice.c
+++ practice.c
@@ -3,7 +3,15 @@
 
 int main()
 {
-     printf("Hello World!\n");
-     return 0;
+     char characterName[] = "John";
+     int  characterAge = 35;
+	
+//     printf("Hello World!\n");
+       printf("There once was a man named %s.\n", characterName);
+       printf("%s was %d years old.\n", characterName, characterAge);
+       printf("He really liked the name %s.\n", characterName);
+       printf("He didn't like being %d though.\n", characterAge);
+   
+       return 0;
 }
 
