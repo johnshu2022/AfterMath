f9494a0f41c39094aa5d8dbf339a195349fd8d25
John <112360755+johnshu2022@users.noreply.github.com>
2023-10-14 16:07:22 +0000

    more practice in c programming

--- practice.c
+++ practice.c
@@ -1,17 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int main()
 {
+     /*
      char characterName[] = "John";
-     int  characterAge = 35;
+     const int  characterAge = 35;
 	
-//     printf("Hello World!\n");
-       printf("There once was a man named %s.\n", characterName);
-       printf("%s was %d years old.\n", characterName, characterAge);
-       printf("He really liked the name %s.\n", characterName);
-       printf("He didn't like being %d though.\n", characterAge);
-   
-       return 0;
+     printf("Hello World!\n");
+     printf("There once was a man named %s.\n", characterName);
+     printf("%s was %d years old.\n", characterName, characterAge);
+     
+     printf("He really liked the name %s.\n", characterName);
+     printf("He didn't like being %d though.\n", characterAge);
+       
+     printf("%f", 10.0 + 12.23);
+     printf("%f", pow(2, 3) );
+       
+     printf("%d\n", characterAge);
+     characterAge = 20;
+     printf("%d", characterAge);
+
+     int age;
+     printf("Enter your age: ");
+     scanf("%d", &age);
+     printf("You are %d years old", age);
+
+     double gpa;
+     printf("Enter your gpa: ");
+     scanf("%lf", &gpa);
+     printf("Your gpa is %f", gpa);
+     
+     */
+
+     char grade;
+     printf("Enter your grade: ");
+     scanf("%c", &grade);
+     printf("Your grade is %c", grade);
+
+     return 0;
 }
 
