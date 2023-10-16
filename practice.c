967c2ffe7c7611f5d60f32d60d4d17cc7d2ae4db
John <112360755+johnshu2022@users.noreply.github.com>
2023-10-16 19:23:28 +0000

    more practice in c programming1

--- practice.c
+++ practice.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <math.h>
 
+void sayHi(){
+	printf("Hello User");
+}
+
 int main()
 {
      /*
@@ -32,13 +36,65 @@ int main()
      scanf("%lf", &gpa);
      printf("Your gpa is %f", gpa);
      
-     */
-
      char grade;
      printf("Enter your grade: ");
      scanf("%c", &grade);
      printf("Your grade is %c", grade);
+     
+     char name[20];
+     printf("Enter your name: ");
+     fgets(name, 20, stdin);
+     printf("Your name is %s", name);
 
+     int num1;
+     int num2;
+
+     printf("Enter first number: ");
+     scanf("%d", &num1);
+     
+     printf("Enter second number: ");
+     scanf("%d", &num2);
+     
+     printf("Answer: %d", num1 + num2);
+
+     double num1;
+     double num2;
+
+     printf("Enter first number: ");
+     scanf("%lf", &num1);
+
+     printf("Enter second number: ");
+     scanf("%lf", &num2);
+
+     printf("Answer: %f", num1 + num2);
+
+     
+
+     char color[20];
+     char pluralNoun[20];
+     char celebrityF[20];
+     char celebrityL[20];
+
+     printf("Enter a color: ");
+     scanf("%s", color);
+
+     printf("Enter a plural noun: ");
+     scanf("%s", pluralNoun);
+     
+     printf("Enter a celebrity: ");
+     scanf("%s%s", celebrityF, celebrityL);
+
+     printf("Roses are %s\n", color);
+     printf("%s are blue\n", pluralNoun);
+     printf("I love %s %s\n", celebrityF, celebrityL);
+
+     int luckyNumbers[] = {7, 13, 21, 25, 32, 39};
+     //luckyNumbers[1] = 17;
+     printf("%d", luckyNumbers[1]);
+     
+     */
+
+     sayHi ();
      return 0;
-}
 
+}
